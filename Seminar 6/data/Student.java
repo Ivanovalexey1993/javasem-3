cd7a0eace53a0a29176e78bb827acae1a690cdb1
ivanovalexey1993 <ivanovalexey1993@mail.ru>
2022-12-29 20:05:59 +0300

    SEMINA6

---
+++ Seminar 6/data/Student.java
@@ -0,0 +1,11 @@
+import java.util.UUID;
+
+public class Student extends Human {
+
+    private UUID studentId;
+
+    public Student(final String firstName, final String secondName, final String patronymic, final UUID studentId) {
+        super(firstName, secondName, patronymic);
+        this.studentId = studentId;
+    }
+}
